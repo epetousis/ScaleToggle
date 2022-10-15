a512e6280a71a511de0a1f317458f65f6947ab06
Evan Petousis <evan@petousis.net>
2022-10-15 15:35:46 +1100

    scaletoggle.py: move scale options to constants

--- scaletoggle.py
+++ scaletoggle.py
@@ -7,6 +7,9 @@
 import dbus
 bus = dbus.SessionBus()
 
+ON_SCALE = 1.5
+OFF_SCALE = 1.0
+
 display_config_well_known_name = "org.gnome.Mutter.DisplayConfig"
 display_config_object_path = "/org/gnome/Mutter/DisplayConfig"
 
@@ -18,7 +21,7 @@ serial, physical_monitors, logical_monitors, properties = display_config_interfa
 updated_logical_monitors=[]
 for x, y, scale, transform, primary, linked_monitors_info, props in logical_monitors:
     if primary == 1:
-        scale = 2.0 if (scale == 1.0) else 1.0 # toggle scaling between 1.0 and 2.0 for the primary monitor
+        scale = ON_SCALE if (scale == OFF_SCALE) else OFF_SCALE # toggle scaling between user set scaling for the primary monitor
     physical_monitors_config = []
     for linked_monitor_connector, linked_monitor_vendor, linked_monitor_product, linked_monitor_serial in linked_monitors_info:
         for monitor_info, monitor_modes, monitor_properties in physical_monitors:
